b0ef7a040b1ddf51a8796f1e5698dd06b372482f
Jian Zhu <36154065+zhujian7@users.noreply.github.com>
2018-07-11 21:03:12 +0800

    Create test.sh (#6)

---
+++ test.sh
@@ -0,0 +1,12 @@
+#/bin/sh
+
+echo " ===== test 1 ===== "
+sleep 3
+echo " ===== test 2 ===== "
+sleep 3
+echo " ===== test 3 ===== "
+sleep 3
+echo " ===== test 4 ===== "
+sleep 3
+echo " ===== test 5 ===== "
+sleep 3
